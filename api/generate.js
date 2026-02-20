16252090822f980e4221111a6f7095ed2c96ef33
diminfomacon-beep <diminfo.macon@gmail.com>
2026-02-19 20:34:53 -0500

    Create generate.js

---
+++ api/generate.js
@@ -0,0 +1,23 @@
+export default async function handler(req,res){
+
+  const {prompt}=req.body;
+
+  const response=await fetch("https://api.openai.com/v1/images/generations",{
+    method:"POST",
+    headers:{
+      "Authorization":`Bearer ${process.env.OPENAI_KEY}`,
+      "Content-Type":"application/json"
+    },
+    body:JSON.stringify({
+      model:"gpt-image-1",
+      prompt:prompt,
+      size:"1024x1024"
+    })
+  });
+
+  const data=await response.json();
+
+  res.status(200).json({
+    url:data.data[0].url
+  });
+}
